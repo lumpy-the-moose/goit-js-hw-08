46360d18209def0c57145f12f4b62164665592ca
lumpy-the-moose <vmaobo@gmail.com>
2022-11-17 19:43:52 +0200

    hw changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,17 @@
-import Player from "@vimeo/player";
-import lodash from "lodash.throttle";
+import Player from '@vimeo/player';
+import lodash from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 player.on(
-  "timeupdate",
-  lodash((data) => {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
-    console.log(localStorage.getItem("videoplayer-current-time"));
+  'timeupdate',
+  lodash(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    console.log(localStorage.getItem('videoplayer-current-time'));
   }, 1000)
 );
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,21 +1,21 @@
-import lodash from "lodash.throttle";
+import lodash from 'lodash.throttle';
 
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 const emailField = document.querySelector('[name="email"]');
 const messageField = document.querySelector('[name="message"]');
 
-let email;
-let message;
+let email = '';
+let message = '';
 
-if (localStorage.getItem("feedback-form-state")) {
-  const savedFeedback = JSON.parse(localStorage.getItem("feedback-form-state"));
-  email = emailField.value = savedFeedback.email;
-  message = messageField.value = savedFeedback.message;
+if (localStorage.getItem('feedback-form-state')) {
+  const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'));
+  email = emailField.value = savedFeedback?.email;
+  message = messageField.value = savedFeedback?.message;
 }
 
 form.addEventListener(
-  "input",
-  lodash((e) => {
+  'input',
+  lodash(e => {
     if (e.target === emailField) {
       email = e.target.value;
     }
@@ -25,18 +25,18 @@ form.addEventListener(
     }
 
     localStorage.setItem(
-      "feedback-form-state",
+      'feedback-form-state',
       JSON.stringify({ email, message })
     );
   }, 500)
 );
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', e => {
   e.preventDefault();
 
   console.log({ email, message });
-  email = "";
-  message = "";
-  localStorage.setItem("feedback-form-state", "");
+  email = '';
+  message = '';
+  localStorage.setItem('feedback-form-state', '');
   form.reset();
 });
